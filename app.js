8bfbbca04e58b190250031a4b0d9af9e74540be1
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-06-19 23:29:01 +0500

    change

--- app.js
+++ app.js
@@ -217,3 +217,12 @@ function scrollToBottom(){
     window.scrollTo(0,document.body.scrollHeight);
 }
 
+document.addEventListener('contextmenu',
+    event => event.preventDefault()
+)
+    document.onkeydown = function(e) {
+        if ((e.keyCode === 67 || e.keyCode === 86 || e.keyCode === 85 || e.keyCode === 123)){
+            alert('Not Allowed Due to Privacy!');
+        }
+        return false;
+};
